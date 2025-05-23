396c04f2be6a5d2346ba1941b6d4a5a8d5b75aab
Pratham Goel <pratham.goel.22.183@gmail.com>
2025-05-24 01:08:15 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import app from './src/app.js';
 import dotenv from 'dotenv';
 dotenv.config();
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+const PORT=process.env.PORT||3001;
+app.listen(PORT, () => {
+  console.log('Server is running on port',PORT);
 });
